807047e1a1f04c383b3a6ea8332ee08e89d7656f
johnsagmani@gmail.com <johnsagmani@gmail.com>
2023-11-05 17:44:36 -0800

    Added files to git repo

---
+++ src/App.tsx
@@ -0,0 +1,162 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  //npm run build after making changes
+  //fetch data from api
+
+  //define types for data being read in
+  interface Team {
+    id: number;
+    name: string;
+    nickname: string;
+    code: string;
+    logo: string;
+  }
+
+  interface Score {
+    win: number;
+    loss: number;
+    series: {
+      win: number;
+      loss: number;
+    };
+    linescore: {
+      0: string;
+      1: string;
+      2: string;
+      3: string;
+    };
+    points: number;
+  }
+
+  interface GameType {
+    id: number;
+    league: string;
+    season: number;
+    date: {
+      start: string;
+      end: null;
+      duration: null;
+    };
+    stage: number;
+    status: {
+      clock: string;
+      halftime: false;
+      short: number;
+      long: string;
+    };
+    periods: {
+      current: number;
+      total: number;
+      endOfPeriod: boolean;
+    };
+    arena: {
+      name: string;
+      city: string;
+      state: string;
+      country: null;
+    };
+    teams: {
+      visitors: Team;
+      home: Team;
+    };
+    scores: {
+      visitors: Score;
+      home: Score;
+    };
+    officials: [];
+    timesTied: null;
+    leadChanges: null;
+    nugget: null;
+  }
+
+  const [game, setGame] = useState<GameType | null>(null);
+
+  async function getLiveStats() {
+    //api endpoint
+    const url = "https://api-nba-v1.p.rapidapi.com/games?live=all";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "bfc7b63475mshe172ddc89e7a5c8p1576dajsn6c8388b25175",
+        "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+
+      //parse data in JSON format
+      const process = JSON.parse(result);
+
+      //if data exists and data is stored in array
+      if (process.response && process.response.length > 0) {
+        //find a game in the array where the Lakers are playing
+        const findGame = process.response.find((lakersGame: GameType) => {
+          return (
+            lakersGame.teams.visitors.nickname === "Lakers" ||
+            lakersGame.teams.home.nickname === "Lakers"
+          );
+        });
+
+        //if lakers game exists, set state variable
+        if (findGame) {
+          setGame(findGame);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getLiveStats();
+  }, []);
+
+  return (
+    <>
+      {game ? (
+        <div>
+          <h2>
+            {game.teams.visitors.nickname} vs. {game.teams.home.nickname}
+          </h2>
+          <h3>
+            {game.periods.current === 4 &&
+            game.status.clock === "0:00" &&
+            game.scores.home.points != game.scores.visitors.points
+              ? "FINAL"
+              : game.periods.current === 4 &&
+                game.status.clock === "0:00" &&
+                game.scores.home.points === game.scores.visitors.points
+              ? `OT ${game.status.clock}`
+              : game.status.halftime
+              ? "HALFTIME"
+              : `QT ${game.periods.current} , ${game.status.clock} left`}
+          </h3>
+          <div>
+            <img
+              src={game.teams.visitors.logo}
+              style={{ width: "40px", height: "20px" }}
+            />
+            {game.teams.visitors.code} {game.scores.visitors.points}
+          </div>
+          <div>
+            <img
+              src={game.teams.home.logo}
+              style={{ width: "40px", height: "20px" }}
+            />
+            {game.teams.home.code} {game.scores.home.points}
+          </div>
+        </div>
+      ) : (
+        <div>
+          No lakers games right now, don't worry though it might be a good
+          thing.
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
